5a42d4e3d17cc338f4b00ae0e28342ccdb226b85
Nazky <56560636+Nazky@users.noreply.github.com>
2021-10-12 16:10:35 +0200

    update and fix some little thing

--- install-arm.sh
+++ install-arm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo mkdir -p /opt/rainpi
 sudo chmod 777 /opt/rainpi
-wget https://assets.checkra.in/downloads/linux/cli/arm/f060f8e27005b553de42b04abd0af6b18eeacd005e68b126e991c6096f51b0b7/checkra1n -O /opt/rainpi/checkra1n
+wget https://assets.checkra.in/downloads/linux/cli/arm/ff05dfb32834c03b88346509aec5ca9916db98de3019adf4201a2a6efe31e9f5/checkra1n -O /opt/rainpi/checkra1n
 sudo chmod +x /opt/rainpi/checkra1n
 sudo cp 01-rainpi.rules /etc/udev/rules.d
 echo "rainpi is now installed!"
